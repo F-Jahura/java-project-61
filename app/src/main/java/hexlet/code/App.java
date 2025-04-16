40e84c9e61612a06951e0ef7162f84cf58bcbbbd
F-Jahura <f_jahura@yahoo.com>
2025-04-16 22:40:19 +0300

    added Even.java and updated App.java and Cli.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,26 @@
 package hexlet.code;
+
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.introduce();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please choose the number and press Enter!");
+        System.out.print("1 - Greet \n2 - Even \n0 - Exit \nYour Choice: ");
+        int number = scanner.nextInt();
+
+        if (number == 1) {
+            System.out.println("\nWelcome to the Brain Games!");
+            Cli.introduce();
+        }
+        if (number == 2) {
+            System.out.println("\nWelcome to the Brain Games!");
+            Cli.introduce();
+            Even.evenNumber();
+        } else if (number == 0) {
+            System.exit(0);
+        }
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,15 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
+    private static String text;
     public static void introduce() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("May I have your name?");
-
-        String text = scanner.nextLine();
+        text = scanner.nextLine();
         System.out.println("Hello, " + text + "!");
+    }
+
+    public static String getText() {
+        return text;
     }
 }
